d62fc893f6c5f90461355ce014b63585a7c0fff4
D9dman <tawhid268@gmail.com>
2023-06-24 14:58:55 +0600

    initial setup with vite-react-ts with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Countries from "./components/Countries";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-7xl mt-20">Rest Countries</h1>
+      <br />
+      <div className="border w-1/2 mx-auto"></div>
+      <Countries></Countries>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Countries.tsx
@@ -0,0 +1,11 @@
+
+
+const Countries = () => {
+    return (
+        <div>
+            <h2 className="countries">Countries</h2>
+        </div>
+    );
+};
+
+export default Countries;
