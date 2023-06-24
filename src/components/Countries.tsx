c8399b5e737bbe1135351ba3d1939a26e665acf4
D9dman <tawhid268@gmail.com>
2023-06-24 18:08:32 +0600

    added filter by oceania and filter by lithunia functionality

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -10,48 +10,90 @@ type Country = {
 const Countries: React.FC = () => {
   const [allCountries, setAllCountries] = useState<Country[]>([]);
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+  const [filteredCountries, setFilteredCountries] = useState<Country[]>([]);
+  const [filterBy, setFilterBy] = useState<string>("");
 
-  const sortCountries = () => {
-    const sortedCountries = [...allCountries].sort((a, b) => {
+  const sortCountries = (countries: Country[]) => {
+    return [...countries].sort((a, b) => {
       if (sortOrder === "asc") {
         return a.name.localeCompare(b.name);
       } else {
         return b.name.localeCompare(a.name);
       }
     });
-    setAllCountries(sortedCountries);
   };
 
   const toggleSortOrder = () => {
-    const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
-    setSortOrder(newSortOrder);
-    sortCountries()
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+
+  const filterCountries = () => {
+    let filtered: Country[] = [];
+    if (filterBy === "oceania") {
+      filtered = allCountries.filter(
+        (country) => country.region === "Oceania"
+      );
+    } else if (filterBy === "smallerThanLithuania") {
+      filtered = allCountries.filter((country) => country.area < 65300);
+    }
+    setFilteredCountries(sortCountries(filtered));
   };
 
   useEffect(() => {
     fetch("https://restcountries.com/v2/all?fields=name,region,area,flag")
       .then((res) => res.json())
-      .then((data) => setAllCountries(data));
+      .then((data) => {
+        const sortedCountries = sortCountries(data);
+        setAllCountries(sortedCountries);
+      });
   }, []);
 
- 
+  useEffect(() => {
+    filterCountries();
+  }, [filterBy]);
+  
+  useEffect(() => {
+    const sortedCountries = sortCountries(allCountries);
+    setFilteredCountries(sortedCountries);
+  }, [sortOrder, allCountries]);
+  const handleFilter = (filter: string) => {
+    setFilterBy(filter);
+  };
 
   return (
     <div>
-      <h2 className="text-center text-4xl">Countries: {allCountries.length}</h2>
-      <div>
-        <div className="ms-10">
-        <label >
-            <span>Sort By : </span>
+      <h2 className="text-center text-4xl">Countries: {filteredCountries.length}</h2>
+      <div className="ms-10">
+        <label>
+          <span>Sort By: </span>
         </label>
         <button
           className="border p-2 rounded-md shadow-md mx-5 bg-blue-600 hover:bg-blue-700 text-white"
           onClick={toggleSortOrder}
         >
-          {sortOrder === "asc" ? "Ascending" : "Descending"}
+          {sortOrder === "asc" ? "Descending" : "Ascending"}
         </button>
-        </div>
-        {allCountries.map(({ name, region, area, flag }) => (
+        <label>
+          <span>Filter By: </span>
+        </label>
+        <button
+          className="border p-2 rounded-md shadow-md mx-5 bg-blue-600 hover:bg-blue-700 text-white"
+          onClick={() => handleFilter("smallerThanLithuania")}
+        >
+          Smaller Area than Lithuania
+        </button>
+        <label>
+          <span>Filter By: </span>
+        </label>
+        <button
+          className="border p-2 rounded-md shadow-md mx-5 bg-blue-600 hover:bg-blue-700 text-white"
+          onClick={() => handleFilter("oceania")}
+        >
+          Oceania
+        </button>
+      </div>
+      <div>
+        {filteredCountries.map(({ name, region, area, flag }) => (
           <div
             className="bg-slate-200 rounded-md p-4 m-5 shadow-lg flex justify-start gap-10 items-center"
             key={name}
